b5c3443cd4f56951f7681b1f3c143e6f627fa076
Edmundo Figueroa <edmundo.figueroaherbas@medirect.com.mt>
2025-08-05 14:40:03 +0200

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3.13
-import sys
-import os
+# import sys
+# import os
 
-# IMPORTANT: Replace 'yourusername' with your actual username
-username = 'figuedmundo' 
-project_name = 'trading-report'
+# # IMPORTANT: Replace 'yourusername' with your actual username
+# username = 'figuedmundo' 
+# project_name = 'trading-report'
 
-# Add your project directory to Python path
-path = f'/home/{username}/{project_name}'
-if path not in sys.path:
-    sys.path.insert(0, path)
+# # Add your project directory to Python path
+# path = f'/home/{username}/{project_name}'
+# if path not in sys.path:
+#     sys.path.insert(0, path)
 
-# Set environment variables
-os.environ['FLASK_APP'] = 'run.py'
-os.environ['PYTHONPATH'] = path
+# # Set environment variables
+# os.environ['FLASK_APP'] = 'run.py'
+# os.environ['PYTHONPATH'] = path
 
-# Python 3.13 specific optimizations
-# Enable the new JIT compiler if available
-os.environ.setdefault('PYTHONUNBUFFERED', '1')
+# # Python 3.13 specific optimizations
+# # Enable the new JIT compiler if available
+# os.environ.setdefault('PYTHONUNBUFFERED', '1')
 
 # Import your Flask application
 from app import create_app
